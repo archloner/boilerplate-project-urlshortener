99b346a45f1658bfa551ddb4fbbe18985edbac0c
philbjern <filipbiernat@gmail.com>
2024-06-25 16:04:00 +0200

    feat: update index.js

--- index.js
+++ index.js
@@ -23,16 +23,13 @@ mongoose.connect(process.env.MONGO_URI, {
 
 let UrlSchema = new mongoose.Schema({
   original_url: String,
-  dns_address: {
-    type: String,
-    unique: true,
-  },
   short_url: String,
 });
 
 let Url = mongoose.model("url_collection", UrlSchema);
 
 const bodyParser = require("body-parser");
+const { randomUUID } = require("crypto");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
@@ -44,7 +41,7 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-const ERROR_RESPONSE = { error: 'invalid url' }
+const ERROR_RESPONSE = { error: "invalid url" };
 
 async function dnsLookup(url) {
   return new Promise(async (resolve, reject) => {
@@ -63,17 +60,18 @@ async function dnsLookup(url) {
       console.log("Empty url: {" + url + "}");
       reject(ERROR_RESPONSE);
     }
-    if (url.includes("/?")) {}
-    
+    if (url.includes("/?")) {
+    }
+
     if (url.includes("ftp:")) {
       console.log("Unsupported protocol: " + url);
       reject(ERROR_RESPONSE);
     }
 
-    let options = {}
+    let options = {};
     options.all = true;
 
-    await dns.lookup(url, (err, addr) => {    
+    await dns.lookup(url, (err, addr) => {
       if (!err || addr != undefined) {
         console.log("DNS lookup sucessfull, address: ");
         console.log(addr);
@@ -87,112 +85,68 @@ async function dnsLookup(url) {
   });
 }
 
-app.post("/api/shorturl/:id", async function (req, res) {
+function validUrl(url) {
+  if (url == "" || url == undefined || url == null) {
+    console.log("Empty url: {" + url + "}");
+    return false;
+  }
+  if (url.includes("ftp:")) {
+    console.log("Unsupported protocol: " + url);
+    return false;
+  }
+
+  return true;
+}
+
+app.post("/api/shorturl", async function (req, res) {
   let url = req.body.url;
   let id = req.params.id;
   let queryParams = req.query.v;
 
   console.log("*** POST " + url + ", id=" + id + ", params=" + queryParams);
+  console.log(req.body)
 
-  dnsLookup(url)
-    .then((addr) => {
-      console.log("dns promise resolved, addr: " + addr);
-      address = addr;
-    })
-    .then(async () => {
-      await Url
-        .find({ dns_address: address })
-        .count()
-        .exec()
-        .then(async (count) => {
-          console.log("Existing items in database count = ");
-          console.log(count);
+  if (!validUrl(url)) {
+    res.json(ERROR_RESPONSE);
+    return;
+  }
 
-          if (count > 0) {
-            console.log("Item already in database");
-            await Url
-              .findOne({ dns_address: address })
-              .exec()
-              .then((doc) => {
-                console.log("Document from database = ")
-                console.log(doc);
-                let obj = {
-                  original_url: doc.get("original_url"),
-                  short_url: doc.get("short_url"),
-                };
+  let doc = new Url({
+    original_url: url,
+    short_url: randomUUID().substring(0, 6),
+  });
+  doc.save();
 
-                return res.json(obj);
-              });
-          } else {
-            console.log("Item not in the database");
-
-            let numberOfElements;
-            Url
-              .findOne()
-              .count()
-              .exec()
-              .then((count) => count)
-              .then(async (d) => {
-                // urlObj = {
-                //   original_url: url,
-                //   dns_address: address,
-                //   short_url: ++d,
-                // };
-                let msg = new Url({ original_url: url, dns_address: address, short_url: ++d });
-                await msg
-                  .save()
-                  .then((doc) => {
-                    console.log('Saving item to database')
-                    console.log(doc);
-                    return res.json({
-                      original_url: doc.original_url,
-                      short_url: doc.short_url,
-                    });
-                  })
-                  .catch((err) => {
-                    console.log("Writing to database error")
-                    console.error(err)
-                  });
-              });
-          }
-        })
-        .catch((err) => {
-          console.log("Item lookup error");
-          console.error("Error: " + err);
-          return res.json(ERROR_RESPONSE);
-        });
-    })
-    .catch((err) => {
-      console.log("dnsLookup catch block (promise rejected)");
-      console.error(err);
-      return res.json(ERROR_RESPONSE);
-    });
+  res.json(doc);
 });
 
-app.get("/api/shorturl/:urlId", (req, res) => {
-  console.log(`**** GET /api/shorturl/:urlId, id=${req.params.urlId}`)
+app.get("/api/shorturl", (req, res) => {
+  return res.json({ msg: "hello" });
+});
+
+app.get("/api/shorturl/:urlId", async (req, res) => {
+  console.log(`*** GET /api/shorturl/:urlId, id=${req.params.urlId}`);
   const id = req.params.urlId;
 
-  if (id !== null && id !== 'undefined') {
-    Url
-      .findOne({ short_url: id })
-      .exec()
-      .then((doc) => {
-        console.log(doc)
-        console.log("URL id found in database " + doc);
-        let address = doc.get("original_url");
-        if (address != null) {
-          if (!address.includes("https://")) {
-            address = "https://" + address;
-          }
-          console.log("Redirecting to url: " + address);
-          res.status(301).redirect(address);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        res.json({ error: "Cannot get the specified url from database" });
-      });
+  if (id === null || id === "undefined") {
+    res.json(ERROR_RESPONSE);
+  }
+
+  try {
+    let doc = await Url.findOne({ short_url: id });
+    
+    console.log(doc);
+    console.log("URL id found in database " + doc);
+
+    let address = doc.original_url;
+    console.log(address);
+    if (address != null) {
+      console.log("Redirecting to url: " + address);
+      res.status(301).redirect(address);
+    }
+  } catch (err) {
+    console.error(err);
+    res.json(ERROR_RESPONSE);
   }
 });
 
