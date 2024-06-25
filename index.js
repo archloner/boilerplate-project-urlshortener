6ab5a2141c715354e56183933bee5ae40c270810
philbjern <filipbiernat@gmail.com>
2024-06-25 12:49:16 +0200

    feat: changes

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use("/public", express.static(`${process.cwd()}/public`));
 
 let mongoose = require("mongoose");
 console.log("connecting to mongodb @ " + process.env.MONGO_URI);
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -29,7 +30,7 @@ let UrlSchema = new mongoose.Schema({
   short_url: String,
 });
 
-let model = mongoose.model("url_collection", UrlSchema);
+let Url = mongoose.model("url_collection", UrlSchema);
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -43,11 +44,11 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-const error_response = { error: 'url invalid' }
+const ERROR_RESPONSE = { error: 'invalid url' }
 
-function dnsLookup(url) {
-  return new Promise((resolve, reject) => {
-    console.log("************\ndnsLookup()\n**********");
+async function dnsLookup(url) {
+  return new Promise(async (resolve, reject) => {
+    console.log("***********\ndnsLookup()\n***********");
     console.log('Path parameter URL = "' + url + '"');
 
     if (url.includes("https://")) {
@@ -60,16 +61,19 @@ function dnsLookup(url) {
     }
     if (url == "" || url == undefined || url == null) {
       console.log("Empty url: {" + url + "}");
+      reject(ERROR_RESPONSE);
     }
-    if (url.includes("/?") || url.includes("ftp:")) {
-      console.log("Empty resource: " + url);
-      reject(error_response);
+    if (url.includes("/?")) {}
+    
+    if (url.includes("ftp:")) {
+      console.log("Unsupported protocol: " + url);
+      reject(ERROR_RESPONSE);
     }
 
     let options = {}
     options.all = true;
 
-    dns.lookup(url, (err, addr) => {    
+    await dns.lookup(url, (err, addr) => {    
       if (!err || addr != undefined) {
         console.log("DNS lookup sucessfull, address: ");
         console.log(addr);
@@ -78,32 +82,35 @@ function dnsLookup(url) {
 
       console.error("DNS lookup failed");
       console.error("Error: " + err);
-      reject(error_response);
+      reject(ERROR_RESPONSE);
     });
   });
 }
 
-app.post("/api/shorturl", function (req, res) {
+app.post("/api/shorturl/:id", async function (req, res) {
   let url = req.body.url;
-  let address;
+  let id = req.params.id;
+  let queryParams = req.query.v;
+
+  console.log("*** POST " + url + ", id=" + id + ", params=" + queryParams);
 
   dnsLookup(url)
     .then((addr) => {
       console.log("dns promise resolved, addr: " + addr);
       address = addr;
     })
-    .then(() => {
-      model
+    .then(async () => {
+      await Url
         .find({ dns_address: address })
         .count()
         .exec()
-        .then((count) => {
+        .then(async (count) => {
           console.log("Existing items in database count = ");
           console.log(count);
 
           if (count > 0) {
             console.log("Item already in database");
-            model
+            await Url
               .findOne({ dns_address: address })
               .exec()
               .then((doc) => {
@@ -120,19 +127,19 @@ app.post("/api/shorturl", function (req, res) {
             console.log("Item not in the database");
 
             let numberOfElements;
-            model
+            Url
               .findOne()
               .count()
               .exec()
               .then((count) => count)
-              .then((d) => {
-                urlObj = {
-                  original_url: url,
-                  dns_address: address,
-                  short_url: ++d,
-                };
-                let msg = new model(urlObj);
-                msg
+              .then(async (d) => {
+                // urlObj = {
+                //   original_url: url,
+                //   dns_address: address,
+                //   short_url: ++d,
+                // };
+                let msg = new Url({ original_url: url, dns_address: address, short_url: ++d });
+                await msg
                   .save()
                   .then((doc) => {
                     console.log('Saving item to database')
@@ -142,31 +149,32 @@ app.post("/api/shorturl", function (req, res) {
                       short_url: doc.short_url,
                     });
                   })
-                  .catch((err) => console.error(err));
+                  .catch((err) => {
+                    console.log("Writing to database error")
+                    console.error(err)
+                  });
               });
           }
         })
         .catch((err) => {
           console.log("Item lookup error");
           console.error("Error: " + err);
-          return res.json(error_response);
+          return res.json(ERROR_RESPONSE);
         });
     })
     .catch((err) => {
       console.log("dnsLookup catch block (promise rejected)");
       console.error(err);
-      return res.json(error_response);
+      return res.json(ERROR_RESPONSE);
     });
 });
 
 app.get("/api/shorturl/:urlId", (req, res) => {
-  console.log("****\nGET /api/shorturl/:urlId\nreq.params.urlId\n*****")
-  console.log(req.params.urlId);
+  console.log(`**** GET /api/shorturl/:urlId, id=${req.params.urlId}`)
   const id = req.params.urlId;
-  console.log(id !== null)
-  console.log(id !== 'undefined')
+
   if (id !== null && id !== 'undefined') {
-    model
+    Url
       .findOne({ short_url: id })
       .exec()
       .then((doc) => {
